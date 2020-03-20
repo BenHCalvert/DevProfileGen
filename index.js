f9ec465af583428b272d33751620660e2c9a73a4
Ben Calvert <benhcalvert@gmail.com>
2020-03-19 18:16:33 -0600

    updates to index.js

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-// Require File System 
-var fs = require("fs");
-// Require Inquirer
-var inquirer = require("inquirer");
-// Require Axios
-const axios = require("axios");
-// require html file
-const htmlProfile = require("./htmlProfile"); 
-// variable to hold questions for prompt
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const open = require("open");
+const convertFactory = require("electron-html-to");
+const api = require("./api");
+const generateHTML = require("./generateHTML");
+
 const questions = [
-    {
-      type: "input",
-      message: "What is your github username",
-      name: "username"
-    },
-    {
+  {
+    type: "input",
+    username: "github",
+    message: "What is your GitHub username?"
+  },
+
+  {
     type: "list",
-    message: "Select your favorite color:",
     name: "color",
-    choices: ["green", "blue-ish", "grey", "purple"]
-    }
-  ];
-// function to prompt user for questions, c-log response & call github API
-function askAboutIt(){
-    inquirer.prompt(questions)
-    .then(function(response) {
-        const gitUser = response.username;
-        const color = response.color;
-        const gitURL = 'https://github.com/';
-        const gitProfile = `${gitURL}${gitUser}`;  
-        console.log(`Thanks! Your favorite color is ${color} and your github url is ${gitProfile}.`);
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-        const starredURL = `https://api.github.com/users/${data.username}/starred`;
+    message: "What is your favorite color?",
+    choices: ["red", "blue", "green", "pink"]
+  }
+];
 
-        axios.get(queryUrl).then(function(response) {
-            
-            fs.writeFile("test.txt", response.name , function(err) {
-              if (err) {
-                throw err;
-              }
-      
-              console.log(`Saved ${response.name} to file`);
-            });
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+  inquirer.prompt(questions).then(({ username, color }) => {
+    console.log("Searching...");
+
+    api
+      .getUser(username)
+      .then(response =>
+        api.getTotalStars(username).then(stars => {
+          return generateHTML({
+            stars,
+            color,
+            ...response.data
           });
-    });
-};
-askAboutIt();
+        })
+      )
+      .then(html => {
+        const conversion = convertFactory({
+          converterPath: convertFactory.converters.PDF
+        });
 
-  
+        conversion({ html }, function(err, result) {
+          if (err) {
+            return console.error(err);
+          }
 
-//add new line to the current file
-// fs.appendFile("log.txt", process.argv[2] + '\n', function(err) {
+          result.stream.pipe(
+            fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+          );
+          conversion.kill();
+        });
 
-//   if (err) {
-//     console.log(err);
-//   }
-//   else {
-//     console.log("Commit logged!");
-//   }
+        open(path.join(process.cwd(), "resume.pdf"));
+      });
+  });
+}
 
-// });
+init();
